273b8a7bae6a627e994ba83fdc3232e3fcbd3bc5
liangchuan <cc14514@icloud.com>
2016-03-17 20:25:16 +0800

    register and login api

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 ps -ef |grep emsg_simple_api|awk '{print $2}'|xargs kill -9
 rm -rf */*.pyc
 rm -rf */*/*.pyc
-uwsgi --http :6080 --socket :6088 --chdir `pwd` --module django_wsgi &
+uwsgi --http :5080 --socket :5088 --chdir `pwd` --module django_wsgi &
